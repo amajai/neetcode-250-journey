2bff46f1be25bceeaf18e59350070d6222872b4d
Abdul <aabdulmajeed.isa@gmail.com>
2025-04-28 13:05:41 +0100

    fix up question 4

--- solutions/01_arrays_and_hashing/04_TwoSum.py
+++ solutions/01_arrays_and_hashing/04_TwoSum.py
@@ -16,48 +16,7 @@ Key Insights:
 - Find the difference and save in it in hashmaps.
 """
 
-"""
-Difficulty: [Easy]
-NeetCode #: [3/250]
-Date Solved: [18/04/25]
-Attempt: [First Attempt]
-Revision: 0
-
-Problem Description:
-Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
-
-An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
-
-Examples:
-
-Example 1:
-
-Input: s = "racecar", t = "carrace"
-
-Output: true
-
-Example 2:
-
-Input: s = "jar", t = "jam"
-
-Output: false
-
-Constraints:
-
-s and t consist of lowercase English letters.
-
-Approach:
-Add characters from both arrays into through own hashmaps and compare them.
-
-Time Complexity: O(n + m)
-Space Complexity: O(1) Because we are at most using 26 characters only.
-
-Key Insights:
-- Using hashmaps for both the arrays
-- Space can be O(1) when there is going to be limited number of space. Example, if string is "sssssssssssssssz", it only going to take up 2 space on hashmap.
-"""
-
-def twoSum(self, nums, target: int):
+def twoSum(nums, target: int):
     prevMap = {} # val: index
 
     for i in range(len(nums)):
